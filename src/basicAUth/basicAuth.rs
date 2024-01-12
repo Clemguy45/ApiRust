eaad3b558ac3bc37902993dbf2b68696f6aef1c0
Clement <o2163453@etu.univ-orleans.fr>
2024-01-12 09:04:39 +0100

    mise en place de Basic Authentication

--- src/basicAUth/basicAuth.rs
+++ src/basicAUth/basicAuth.rs
@@ -1,4 +1,30 @@
-struct basicAuth{
+
+struct BasicAuth{
      username: String,
      password: String
+}
+
+impl BasicAuth{
+     fn from_auth_header(header : &str) -> Option<BasicAuth>{
+         let split = header.split_ascii_whitespace().collect::<Vec<&str>>();
+         if split.len() != 2 || split[0] != "Basic"{
+             return None;
+         }
+          Self::from_base64(split[1])
+     }
+
+     fn from_base64(base64_string : &str) -> Option<BasicAuth>{
+         let decoded = base64::decode(base64_string).ok()?;
+         let decoded_str = String::from_utf8(decoded).ok()?;
+         let split = decoded_str.split(':').collect::<Vec<&str>>();
+
+         if split.len() != 2{
+             return None;
+         }
+         let (username, password) = (split[0].to_string(), split[1].to_string());
+
+         Some(BasicAuth{
+             username,
+             password})
+     }
 }
--- src/basicAUth/mod.rs
+++ src/basicAUth/mod.rs
@@ -1,1 +1,1 @@
-pub mod basicAuth;
+pub mod BasicAuth;
